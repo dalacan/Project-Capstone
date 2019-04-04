e07fcd16f96208340e096c0d143b9ed430b957b8
Derrick Choo <dalacan@gmail.com>
2019-04-05 04:56:29 +1100

    Updated braking torque to required torque by the real car to stop.

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -75,7 +75,7 @@ class Controller(object):
 
         if linear_vel == 0. and current_vel < 0.1:
             throttle = 0
-            brake = 400 #N*m - to hold the car in place if we are stopped at a light. Accl - 1m/s^2
+            brake = 700 #N*m - to hold the car in place if we are stopped at a light. Accl - 1m/s^2
         elif throttle < .1 and vel_error <0:
             throttle = 0
             decel = max(vel_error, self.decel_limit)
