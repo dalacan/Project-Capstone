229d08d38fede67a8cc36f176637950858cb18f7
Derrick Choo <dalacan@gmail.com>
2019-04-05 04:54:22 +1100

    Fixed traffic light detector to use rgb encoding instead of bgr.

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -156,7 +156,7 @@ class TLDetector(object):
             self.prev_light_loc = None
             return False
 
-        cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
+        cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "rgb8")
 
         #Get classification, return the state
         return self.light_classifier.get_classification(cv_image)
